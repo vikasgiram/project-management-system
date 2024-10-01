f103a0ca97eb5727acddb67f40190c2efe8d6cc1
sushil patil <patilsushil1085@gmail.com>
2024-10-01 15:50:45 +0530

    Fix Date in Update fild ProjectMaster

--- frontend/src/utils/formatDate.js
+++ frontend/src/utils/formatDate.js
@@ -9,3 +9,16 @@ export function formatDate(dateString) {
     const year = date.getFullYear();
     return `${day} ${month} ${year}`;
   }
+
+  export function formatDateforupdate(dateString) {
+    /**
+     * Format a date string from ISO 8601 to MM/DD/YYYY
+     */
+    const date = new Date(dateString);
+    const day = String(date.getDate()).padStart(2, '0'); // Ensure 2 digits for day
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // Ensure 2 digits for month
+    const year = date.getFullYear();
+
+    // Format the date as MM/DD/YYYY
+    return `${year}-${month}-${day}`;
+}
