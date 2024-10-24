205de6aef466d9cd6cd28d14a4054875ef1c5889
Vicky <vikigiram@gmail.com>
2024-10-24 11:41:53 +0530

    hookes created for the admin

---
+++ frontend/src/hooks/useAdmin.js
@@ -0,0 +1,89 @@
+import axios from 'axios';
+import toast from 'react-hot-toast';
+
+const url="api/admin";
+
+const getAdmin = async () => {
+  try {
+    const response = await axios.get(`${url}`);
+    const data = response.data;
+
+    if (data.error) {
+      console.error(data.error);
+      return alert(data.error);
+    }
+
+    return data;
+  } catch (error) {
+    console.error(error);
+    toast.error("Internal Server Error");  }
+};
+
+
+
+const createAdmin = async (adminData) => {
+  try {
+    const response = await axios.post(`${url}`, adminData);
+    const data = response.data;
+
+    if (data.error) {
+      console.error(data.error);
+      return toast.error(data.error);
+    }
+
+    toast.success("Admin Created");
+  } catch (error) {
+    console.error(error);
+    toast.error("Internal Server Error");  }
+};
+
+const updateAdmin = async (updatedAdminData) => {
+  try {
+    const response = await axios.put(`${url}/${updatedAdminData._id}`, updatedAdminData);
+    const data = response.data;
+
+    if (data.error) {
+      console.error(data.error);
+      return toast.error(data.error);
+    }
+    toast.success("Admin Updated sucessfully...");
+    return data;
+  } catch (error) {
+    console.error(error);
+    toast.error("Internal Server Error");  }
+};
+
+const deleteAdmin = async (id) => {
+  try {
+    const response = await axios.delete(`${url}/${id}`);
+    const data = response.data;
+
+    if (data.error) {
+      console.error(data.error);
+      return alert(data.error);
+    }
+
+    return data;
+  } catch (error) {
+    console.error(error);
+    toast.error("Internal Server Error");  }
+};
+
+const getAdminDashboard = async () => {
+  try {
+    const response = await axios.get(`${url}/dashboard`);
+    const data = response.data;
+
+    if (data.error) {
+      console.error(data.error);
+      return toast.error(data.error);
+    }
+
+    return data;
+  } catch (error) {
+    console.error(error);
+    toast.error("Internal Server Error");  }
+};
+
+  
+export { getAdmin, createAdmin, updateAdmin, deleteAdmin, getAdminDashboard};
