6c8efd1ee3a3a5bfe39495743ef6239d14966993
Akash <uxdesigner.daccess.@gmail.com>
2024-10-29 11:06:09 +0530

    hsgsdcdg

--- frontend/src/Components/Private/EmployeeDashboard/EmployeeDashboardPieChart.js
+++ frontend/src/Components/Private/EmployeeDashboard/EmployeeDashboardPieChart.js
@@ -20,6 +20,8 @@ export const CompanyInfEmployeeDashboardPieChartoPieChart = ({completedProjectCo
       width: "100%",
       backgroundColor: {
         opacity: 0
+
+        
       },
     },
     slices: { 0: { color: '#80C783' }, 1: { color: '#ebc682' }} //56AFFE
